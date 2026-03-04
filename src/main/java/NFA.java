14657777a67910823c33fc622e5ddbf1ba03e3c3
PolCaballe <polcaballe.a@gmail.com>
2026-03-04 13:04:11 +0100

    Implementacio parcial

--- src/main/java/NFA.java
+++ src/main/java/NFA.java
@@ -1,49 +1,55 @@
 import java.util.*;
 
 public class NFA {
-    
+
+    String estatInicial;
+    String[] states;
+    String[] estatsFinals;
+    int numEFinals = 0;
+    Map<String, Map<Character, String>> taulaTransicions = new HashMap<>();
+
     public NFA(String[] states) {
-        //your code goes here
+        estatsFinals = new String[states.length];
+        this.states = states;
     }
 
     public void setInitialState(String state) {
-        // your code goes here
+        estatInicial = state;
     }
 
     public String getInitialState() {
-        // your code goes here
-        return null;
+        return estatInicial;
     }
 
     public void addFinalState(String state) {
-        // your code goes here
+        estatsFinals[numEFinals++] = state;
     }
 
     // Method to add a transition to the table
     public void addTransition(String state, Character input, String nextState) {
-        // your code goes here
+        taulaTransicions.putIfAbsent(state, new HashMap<>());
+        taulaTransicions.get(state).put(input, nextState);
     }
 
     public void addEpsilonTransition(String state, String nextState) {
-        // your code goes here
+        addTransition(state, null, nextState);
     }
 
     // Outputs true if the NFA accept the input string
-    //The strategy we follow is to convert the NFA into a DFA
+    // The strategy we follow is to convert the NFA into a DFA
     public boolean accept(String input) {
         // your code goes here
         return false;
     }
 
-    public DFA NFAtoDFA(){
-        // your code goes here
-        return null;
-     }
-
-    public Set<Label> getAllLabels(){
+    public DFA NFAtoDFA() {
         // your code goes here
         return null;
     }
-                    
-       
+
+    public Set<Label> getAllLabels() {
+        // your code goes here
+        return null;
+    }
+
 }
